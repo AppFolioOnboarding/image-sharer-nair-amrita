c18378f60527c38feb43d245c49cb6030ca8b88c
Amrita Nair <amrita.nair@appfolio.com>
2020-07-27 15:15:05 -0400

    an - Added tests and Removed unwanted comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,35 +32,3 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :image)
   end
 end
-
-# requests are received by Rails
-# e.g. GET /posts/new
-#
-# Rails tries to find a matching ROUTE
-# routes are defined in config/routes.rb
-# -> Which controller and method should we pass this request to?
-#
-# Routes are matched by path (aka URL) and HTTP method
-# In this example we are looking for a route that matches
-# GET /posts/new
-#
-# Defining routes:
-# HTTP_VERB 'URL/PATH'
-#
-# get 'posts/new'
-# GET /posts/new -> PostsController#new
-#
-# index 'posts'
-# GET /posts -> PostsController#index
-#
-# resources(:posts)
-# this defines the 7 common restful routes for "Post"
-
-# Once we make it to a controller -- generate a response
-#
-# class PostsController
-#  def show
-#
-#    render :show
-#  end
-# end
