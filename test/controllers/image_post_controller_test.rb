cde37fc172e3403e4e8cdb42744248eceae265ac
Amrita Nair <amrita.nair@appfolio.com>
2020-07-28 15:31:45 -0400

    an - Minor changes

--- test/controllers/image_post_controller_test.rb
+++ test/controllers/image_post_controller_test.rb
@@ -17,17 +17,4 @@ class ImagePostControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
   end
-
-  test 'Cannot successfully create a post with no image attached' do
-    assert_raise ActiveRecord::RecordInvalid do
-      Post.create!(title: 'Newest')
-    end
-  end
-
-  test 'Cannot successfully create a post with attachments other than image' do
-    assert_raise ActiveRecord::RecordInvalid do
-      file = fixture_file_upload(Rails.root.join('public', 'dummy.pdf'), 'application/pdf')
-      Post.create!(title: 'Newest', image: file)
-    end
-  end
 end
