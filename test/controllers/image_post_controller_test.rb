15b6ab604c7bd72bc0154636b55befd7fbd01c03
Amrita Nair <amrita.nair@appfolio.com>
2020-07-28 16:23:38 -0400

    an - Refactored tests

--- test/controllers/image_post_controller_test.rb
+++ test/controllers/image_post_controller_test.rb
@@ -3,17 +3,17 @@ require 'open-uri'
 
 class ImagePostControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get posts_path
+    get image_posts_path
     assert_response :success
   end
 
   test 'should create image' do
-    assert_difference 'Post.count', 1 do
+    assert_difference 'ImagePost.count', 1 do
       file = fixture_file_upload(Rails.root.join('public', 'images.jpeg'), 'image/jpeg')
-      post posts_path, params: { post: { title: 'Image1', image: file } }
+      post image_posts_path, params: { image_post: { title: 'Image1', image: file } }
     end
 
-    assert_redirected_to post_path(Post.last.id)
+    assert_redirected_to image_post_path(ImagePost.last.id)
     follow_redirect!
     assert_response :success
   end
--- test/models/image_post_model_test.rb
+++ test/models/image_post_model_test.rb
@@ -4,14 +4,14 @@ require 'open-uri'
 class ImagePostTest < ActionDispatch::IntegrationTest
   test 'Cannot successfully create a post with no image attached' do
     assert_raise ActiveRecord::RecordInvalid do
-      Post.create!(title: 'Newest')
+      ImagePost.create!(title: 'Newest')
     end
   end
 
   test 'Cannot successfully create a post with attachments other than image' do
     assert_raise ActiveRecord::RecordInvalid do
       file = fixture_file_upload(Rails.root.join('public', 'dummy.pdf'), 'application/pdf')
-      Post.create!(title: 'Newest', image: file)
+      ImagePost.create!(title: 'Newest', image: file)
     end
   end
 end
