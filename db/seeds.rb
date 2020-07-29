d9baf49e6f7121b7b1eb160910800ff9e8cce692
Amrita Nair <amrita.nair@appfolio.com>
2020-07-29 16:07:45 -0400

    an - Added seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+20.times do |n|
+  image_post = ImagePost.create(title: "Image#{n}")
+  image_url = "https://picsum.photos/id/#{n}/200/300"
+  p "Downloading #{image_url}..."
+  downloaded_file = URI.parse(image_url).open
+  image_post.image.attach(io: downloaded_file, filename: 'foo.png')
+  image_post.save
+  p "Created #{image_post.title} with id: #{image_post.id}"
+end
+p "Created #{ImagePost.count} image posts"
