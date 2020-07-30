eeeb51d45ec0afabfe0b23e24a265eafd56e99b8
Amrita Nair <amrita.nair@appfolio.com>
2020-07-30 15:34:16 -0400

    an - Added model tests for tags

--- test/models/image_post_model_test.rb
+++ test/models/image_post_model_test.rb
@@ -14,4 +14,30 @@ class ImagePostTest < ActionDispatch::IntegrationTest
       ImagePost.create!(title: 'Newest', image: file)
     end
   end
+
+  test 'create post with tags' do
+    file = fixture_file_upload(Rails.root.join('public', 'images.jpeg'), 'image/jpeg')
+    ImagePost.create!(title: 'Newest', image: file, tag_list: 'tag_1')
+    assert_equal 1, ImagePost.last.tags.count
+    assert_equal 'tag_1', ImagePost.last.tags.first.name
+  end
+
+  test 'update tags of a post' do
+    file = fixture_file_upload(Rails.root.join('public', 'images.jpeg'), 'image/jpeg')
+    ImagePost.create!(title: 'Newest', image: file, tag_list: 'tag_1')
+    post = ImagePost.last
+    post.tag_list.add('tag_2')
+    post.save
+    assert_equal 2, post.tags.count
+  end
+
+  test 'delete tags of a post' do
+    file = fixture_file_upload(Rails.root.join('public', 'images.jpeg'), 'image/jpeg')
+    ImagePost.create!(title: 'Newest', image: file, tag_list: 'tag_1, tag_2')
+    post = ImagePost.last
+    assert_equal 2, post.tags.count
+    post.tag_list.delete('tag_1')
+    post.save
+    assert_equal 1, post.tags.count
+  end
 end
